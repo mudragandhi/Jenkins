1e1c3fd6f49c25db5ca3012e1894e33bde0838a5
Mudra Gandhi <JZ0996@NAM.corp.gm.com>
2017-10-10 14:17:36 -0500

    fixed issue to run test2 successfully

--- compute_highest_affinity.py
+++ compute_highest_affinity.py
@@ -1,23 +1,38 @@
 import operator
 
+import itertools
+from collections import Counter
+
 
 class HighestWebPagesAffinity:
+
     def highest_affinity(self, site_list, user_list, time_list):
-        user_set = set(user_list)
+        site_groups = Counter(site_list)
+        sorted_by_access = sorted(site_groups.items(), key=operator.itemgetter(1), reverse=True)
         data_dict = {}
-        for i in range(len(site_list)):
-            p = site_list[i]
-            for j in range(1, len(site_list)):
-                q = site_list[j]
+        previous_affinity_count = 0
+        if len(sorted_by_access) < 2:
+            print('input size should be greater than 2')
+            return
+
+        for i in range(len(sorted_by_access)):
+            for j in range(i, len(sorted_by_access)):
+                p = sorted_by_access[i][0]
+                q = sorted_by_access[j][0]
                 if p is not q:
                     if (p, q) not in data_dict and (q, p) not in data_dict:
-                        count_of_users_for_pair = 0
-                        for user in user_set:
-                            indices = [i for i, x in enumerate(user_list) if x == user]
-                            a = [site_list[i] for i in indices]
-                            if p in a and q in a:
-                                count_of_users_for_pair += 1
-                                data_dict.__setitem__((p, q), count_of_users_for_pair)
+                        indices_of_p = [i for i, x in enumerate(site_list) if x == p]
+                        indices_of_q = [i for i, x in enumerate(site_list) if x == q]
+                        users_of_p = [user_list[i] for i in indices_of_p]
+                        users_of_q = [user_list[i] for i in indices_of_q]
+                        count_of_users_for_pair = len(set(users_of_p) & set(users_of_q))
+                        dict_key = [p, q]
+                        dict_key.sort()
+                        data_dict.__setitem__((dict_key[0], dict_key[1]), count_of_users_for_pair)
+                        if previous_affinity_count >= count_of_users_for_pair:
+                            return max(data_dict.items(), key=operator.itemgetter(1))[0]
+                        else:
+                            previous_affinity_count = count_of_users_for_pair
         return max(data_dict.items(), key=operator.itemgetter(1))[0]
 
 
